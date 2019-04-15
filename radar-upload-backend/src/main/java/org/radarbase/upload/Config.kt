86b8580898fd7ed853674505404d619a640eea70
nivethika <nivemaham@gmail.com>
2019-04-15 12:44:06 +0200

    Rename UploadResource to resourceConfig, since the name is confusing since we also can rest resources as resources

--- radar-upload-backend/src/main/java/org/radarbase/upload/Config.kt
+++ radar-upload-backend/src/main/java/org/radarbase/upload/Config.kt
@@ -1,12 +1,12 @@
 package org.radarbase.upload
 
-import org.radarbase.upload.inject.ManagementPortalResources
+import org.radarbase.upload.inject.ManagementPortalResourceConfig
 import java.net.URI
 
 data class Config(
         var baseUri: URI = URI.create("http://0.0.0.0:8080/radar-upload/"),
         var managementPortalUrl: String = "http://managementportal-app:8080/managementportal/",
-        var resourceConfig: Class<*> = ManagementPortalResources::class.java,
+        var resourceConfig: Class<*> = ManagementPortalResourceConfig::class.java,
         var jdbcDriver: String? = "org.h2.Driver",
         var jdbcUrl: String? = null,
         var jdbcUser: String? = null,
