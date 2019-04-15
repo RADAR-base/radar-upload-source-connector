45af4bf3fa474121517fad33d22af2d30bd0daa0
nivethika <nivemaham@gmail.com>
2019-04-15 12:42:50 +0200

    moving DTOs/API declarations to same file, since it is much more convenient to see all declarations at once and it suits the Kotlin coding convention

--- radar-upload-backend/src/main/java/org/radarbase/upload/dto/ContentsDTO.kt
+++
@@ -1,5 +0,0 @@
-package org.radarbase.upload.dto
-
-import java.time.Instant
-
-data class ContentsDTO(var url: String? = null, var text: String? = null, var contentType: String, var createdDate: Instant, var size: Long, var fileName: String)
--- radar-upload-backend/src/main/java/org/radarbase/upload/dto/RecordMetadataDTO.kt
+++
@@ -1,5 +0,0 @@
-package org.radarbase.upload.dto
-
-import java.time.Instant
-
-data class RecordMetadataDTO(var revision: Int, var status: String, var message: String?, var createdDate: Instant, var modifiedDate: Instant, var committedDate: Instant?, var logs: LogsDto? = null)
