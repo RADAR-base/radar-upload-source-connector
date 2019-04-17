388ae08ba34c9428cee6601c3abe56ee83b2f3f4
Joris Borgdorff <github@blootsvoets.net>
2019-04-17 13:59:28 +0200

    Small repo API change

--- radar-upload-backend/src/main/java/org/radarbase/upload/doa/SourceTypeRepositoryImpl.kt
+++ radar-upload-backend/src/main/java/org/radarbase/upload/doa/SourceTypeRepositoryImpl.kt
@@ -12,33 +12,37 @@ class SourceTypeRepositoryImpl(@Context private var em: EntityManager) : SourceT
     override fun create(record: SourceType) = em.transact { persist(record) }
 
     override fun readAll(limit: Int?, lastId: Long?, detailed: Boolean): List<SourceType>  {
-        var queryString = "SELECT s FROM SourceType s WHERE s.id > :lastId "
+        var queryString = "SELECT s FROM SourceType s"
+        lastId?.let {
+            queryString += " WHERE s.id > :lastId "
+        }
         if (detailed) {
             queryString += " JOIN FETCH s.configuration"
         }
         queryString += "ORDER BY s.id"
 
         return em.transact {
-            val query = createQuery(queryString, SourceType::class.java)
-            limit?.let {
-                query.setMaxResults(it)
+            createQuery(queryString, SourceType::class.java).run {
+                limit?.let {
+                    maxResults = it
+                }
+                lastId?.let {
+                    setParameter("lastId", it)
+                }
+                logger.info("QUERY PARAMETERS: {}", parameters)
+                resultList
             }
-            lastId?.let {
-                query.setParameter("lastId", it)
-            }
-
-            logger.info("QUERY PARAMETERS: {}", query.parameters)
-            query.resultList
         }
     }
 
     override fun read(name: String): SourceType? = em.transact {
-        val queryString = "SELECT s FROM SourceType s JOIN FETCH s.configuration WHERE  s.name = :name"
+        val queryString = "SELECT s FROM SourceType s JOIN FETCH s.configuration WHERE s.name = :name"
 
-        val query = createQuery(queryString, SourceType::class.java)
-                .setParameter("name", name)
-        logger.info("Query Source type with name $name : {}", query.parameters)
-        query.resultList.firstOrNull()
+        createQuery(queryString, SourceType::class.java).run {
+            setParameter("name", name)
+            logger.info("Query Source type with name $name : {}", parameters)
+            resultList.firstOrNull()
+        }
     }
 
     override fun update(record: SourceType): SourceType = em.transact { merge<SourceType>(record) }
