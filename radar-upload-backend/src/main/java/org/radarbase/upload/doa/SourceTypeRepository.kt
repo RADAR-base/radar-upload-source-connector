aa8bd9853b539fda58bed45ffcbb837136039b58
Joris Borgdorff <github@blootsvoets.net>
2019-04-17 13:59:28 +0200

    Small repo API change

--- radar-upload-backend/src/main/java/org/radarbase/upload/doa/SourceTypeRepository.kt
+++ radar-upload-backend/src/main/java/org/radarbase/upload/doa/SourceTypeRepository.kt
@@ -5,7 +5,7 @@ import org.radarbase.upload.doa.entity.SourceType
 interface SourceTypeRepository {
     fun create(record: SourceType)
     fun read(name: String): SourceType?
-    fun readAll(limit: Int? = 20, lastId: Long? = -1L, detailed: Boolean = false): List<SourceType>
+    fun readAll(limit: Int? = null, lastId: Long? = null, detailed: Boolean = false): List<SourceType>
     fun delete(record: SourceType)
     fun update(record: SourceType): SourceType
 }
