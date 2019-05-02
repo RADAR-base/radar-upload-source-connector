9ff4651512f18101565d46f5ac326e7bcad85517
nivethika <nivemaham@gmail.com>
2019-05-02 16:12:38 +0200

    add a sample csv-converter

---
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/AccelerometerCsvRecordConverter.kt
@@ -0,0 +1,31 @@
+package org.radarbase.connect.upload.converter
+
+import org.radarbase.connect.upload.api.RecordDTO
+import org.radarbase.connect.upload.api.UploadBackendClient
+import org.radarcns.passive.phone.PhoneAcceleration
+
+class AccelerometerCsvRecordConverter(override val sourceType: String = "phone-acceleration")
+    : CsvRecordConverter(sourceType) {
+
+    override fun validateHeaderSchema(csvHeader: List<String>) =
+        listOf("TIMESTAMP", "X", "Y", "Z").equals(csvHeader)
+
+
+    override fun convertLineToRecord(lineValues: Map<String, String>, timeReceived: Double, topic: String): TopicData {
+        val time = lineValues["TIMESTAMP"]?.toDouble()
+        val acceleration = PhoneAcceleration(
+                time,
+                timeReceived,
+                lineValues["X"]?.toFloat(),
+                lineValues["Y"]?.toFloat(),
+                lineValues["Z"]?.toFloat()
+                )
+
+        return TopicData(false, topic, acceleration)
+    }
+
+    override fun commitLogs(record: RecordDTO, client: UploadBackendClient) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
