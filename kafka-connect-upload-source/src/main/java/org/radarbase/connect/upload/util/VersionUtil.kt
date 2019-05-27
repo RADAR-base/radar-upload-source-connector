d5c95726e7f86762e500dab94f8d30d4c65caecf
nivethika <nivemaham@gmail.com>
2019-05-27 16:57:59 +0200

    fix properties and config

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/util/VersionUtil.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/util/VersionUtil.kt
@@ -3,6 +3,6 @@ package org.radarbase.connect.upload.util
 
 class VersionUtil {
     companion object {
-        fun getVersion() = VersionUtil::class.java.`package`.implementationVersion
+        fun getVersion(): String = VersionUtil::class.java.`package`.implementationVersion ?: "0.0.0.0"
     }
 }
