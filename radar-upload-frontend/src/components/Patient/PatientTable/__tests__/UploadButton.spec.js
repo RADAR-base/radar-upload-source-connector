f3c4ba40767af568772f5b49c5e3870d1186c195
Long <khoathanglong@gmail.com>
2019-08-28 11:19:07 +0200

    modify UploadButton test

--- radar-upload-frontend/src/components/Patient/PatientTable/__tests__/UploadButton.spec.js
+++ radar-upload-frontend/src/components/Patient/PatientTable/__tests__/UploadButton.spec.js
@@ -2,6 +2,7 @@
 import { shallowMount } from '@vue/test-utils';
 import flushPromises from 'flush-promises';
 import { Store } from 'vuex-mock-store';
+import { wrap } from 'module';
 import UploadButton from '../UploadButton.vue';
 import fileAPI from '@/axios/file.js';
 
@@ -35,15 +36,7 @@ describe('UploadButton', () => {
     stubs: ['v-btn',
       'v-icon',
       'v-menu',
-      'v-card',
-      'v-list',
-      'v-subheader',
-      'v-card-actions',
-      'v-spacer',
-      'v-card-text',
-      'v-list-item',
-      'v-select',
-      'v-file-input'],
+    ],
   });
 
   beforeEach(() => {
@@ -53,73 +46,26 @@ describe('UploadButton', () => {
   it('has uploadInfo props', () => {
     expect(wrapper.vm.uploadInfo).toEqual(postRecordBody);
   });
-  it('call api to get sourceTypes/resourceTypes and contentTypes when created', async () => {
-    expect(wrapper.vm.sourceTypeList).toEqual(sourceTypeList.map(el => el.name));
-    expect(wrapper.vm.contentTypes).toEqual(sourceTypeList.map(el => el.contentTypes));
-  });
 
-  it('upload btn disabled if either file or sourceType not selected', () => {
-    const file = new File([''], 'filename1');
-    const uploadButton = wrapper.findAll('v-btn-stub').at(1);
-    expect(uploadButton.attributes().disabled).toBe('true');
-    wrapper.setData({ sourceType: '123' });
-    expect(uploadButton.attributes().disabled).toBe('true');
-    wrapper.setData({ file });
-    expect(uploadButton.attributes().disabled).not.toBe('true');
-  });
-
-  it('upload files success', async () => {
-    // mock POST /records
-    const postReturnVal = { id: 'id1', createdDate: '2019-10-10' };
-    const { projectId, userId } = wrapper.vm.uploadInfo;
-    const postRecordPayload = { projectId, userId, sourceType: wrapper.vm.sourceType };
-    fileAPI.postRecords = jest.fn().mockResolvedValue(postReturnVal);
-    // mock PUT request
-    const { file } = wrapper.vm;
-    const putRecordPayload = { id: postReturnVal.id, file, fileName: file.name };
-    const putReturnVal = { id: 'id1' };
-    fileAPI.putRecords = jest.fn().mockResolvedValue(putReturnVal);
-    // file uploaded
-    const files = [];
-    files.push({ fileName: file.name, uploading: true });
-
-
-    const uploadButton = wrapper.findAll('v-btn-stub').at(1);
-    uploadButton.trigger('click');
-    await flushPromises();
-
-    // postRecords
-    expect(fileAPI.postRecords).toBeCalledWith(postRecordPayload);
-    // eslint-disable-next-line max-len
-    expect(wrapper.emitted().startUploading[0][0]).toEqual({ ...postReturnVal, files, active: true });
-    expect(wrapper.vm.menu).toBe(false);
-
-    // putRecords
-    expect(fileAPI.putRecords).toBeCalledWith(putRecordPayload);
-    expect(wrapper.emitted().addUploadingFile[0][0]).toEqual(putReturnVal);
-  });
-
-  it('uploadfile error', async () => {
-    wrapper.setData({ menu: true });
-    fileAPI.postRecords.mockRejectedValue();
-    const uploadButton = wrapper.findAll('v-btn-stub').at(1);
-    uploadButton.trigger('click');
-    await flushPromises();
-    expect(wrapper.vm.menu).toBe(false);
-    expect(wrapper.vm.$error).toBeCalled();
-  });
-
-  it('close menu on cancel click', () => {
-    const cancelBtn = wrapper.findAll('v-btn-stub').at(0);
-    cancelBtn.trigger('click');
+  it('finishUpload', () => {
+    wrapper.setData({ menu: true, loading: true });
+    const file = {};
+    wrapper.vm.finishUpload(file);
+    expect(wrapper.vm.loading).toBe(false);
+    expect(wrapper.emitted().addUploadingFile[0][0]).toEqual(file);
     expect(wrapper.vm.menu).toBe(false);
   });
 
-  it('remove data when menu closes', () => {
-    wrapper.setData({ menu: true });
-    wrapper.setData({ menu: false });
-    expect(wrapper.vm.file).toEqual([]);
-    expect(wrapper.vm.sourceType).toBe('');
-    expect(wrapper.vm.sourceTypeList).toEqual([]);
+  it('startUploading', () => {
+    const payload = '';
+    wrapper.vm.startUploading(payload);
+    expect(wrapper.emitted().startUploading[0][0]).toEqual(payload);
+  });
+
+  it('closeMenu', () => {
+    wrapper.setData({ menu: true, loading: true });
+    wrapper.vm.closeMenu();
+    expect(wrapper.vm.loading).toBe(false);
+    expect(wrapper.vm.menu).toBe(false);
   });
 });
