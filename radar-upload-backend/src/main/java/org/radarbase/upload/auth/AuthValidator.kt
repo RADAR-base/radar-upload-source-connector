daababe643b5c5c269520261c9c4770fc2e0ab1d
Joris Borgdorff <github@blootsvoets.net>
2019-08-29 15:37:59 +0200

    Allow backend to read authorization header from cookie
    This allows direct downloads to go through without waiting.

--- radar-upload-backend/src/main/java/org/radarbase/upload/auth/AuthValidator.kt
+++ radar-upload-backend/src/main/java/org/radarbase/upload/auth/AuthValidator.kt
@@ -32,12 +32,17 @@ interface AuthValidator {
         val authorizationHeader = request.getHeaderString("Authorization")
 
         // Check if the HTTP Authorization header is present and formatted correctly
-        if (authorizationHeader == null
-                || !authorizationHeader.startsWith(AuthenticationFilter.BEARER, ignoreCase = true)) {
-            return null
+        if (authorizationHeader != null
+                && !authorizationHeader.startsWith(AuthenticationFilter.BEARER, ignoreCase = true)) {
+            return authorizationHeader.substring(AuthenticationFilter.BEARER.length).trim { it <= ' ' }
+        }
+
+        val authorizationCookie = request.cookies["authorizationBearer"]
+        if (authorizationCookie != null) {
+            return authorizationCookie.value
         }
 
         // Extract the token from the HTTP Authorization header
-        return authorizationHeader.substring(AuthenticationFilter.BEARER.length).trim { it <= ' ' }
+        return null
     }
 }
