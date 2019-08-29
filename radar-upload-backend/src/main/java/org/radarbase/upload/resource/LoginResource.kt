679127c0d35d67de55e63e680e59efb0144753ce
Joris Borgdorff <github@blootsvoets.net>
2019-08-29 16:21:21 +0200

    Allow redirects

--- radar-upload-backend/src/main/java/org/radarbase/upload/resource/LoginResource.kt
+++ radar-upload-backend/src/main/java/org/radarbase/upload/resource/LoginResource.kt
@@ -3,24 +3,26 @@ package org.radarbase.upload.resource
 import org.radarbase.upload.Config
 import org.radarbase.upload.auth.Auth
 import org.radarbase.upload.auth.Authenticated
+import java.net.URI
 import javax.annotation.Resource
-import javax.ws.rs.BadRequestException
-import javax.ws.rs.POST
-import javax.ws.rs.Path
+import javax.ws.rs.*
 import javax.ws.rs.core.*
 
 @Authenticated
 @Path("/login")
 @Resource
 class LoginResource {
-    @POST
-    fun login(@Context auth: Auth, @Context config: Config, @Context uri: UriInfo): Response {
+    @GET
+    fun login(@QueryParam("redirect") redirect: String?, @Context auth: Auth, @Context config: Config, @Context uri: UriInfo): Response {
         val token = auth.bearerToken
                 ?: throw BadRequestException("Cannot log in without bearer token")
 
         val myUri = config.advertisedBaseUri ?: uri.baseUri
 
-        return Response.noContent()
+        val responseBuilder = redirect?.let { Response.temporaryRedirect(URI.create(it)) }
+                ?: Response.noContent()
+
+        return responseBuilder
                 .cookie(NewCookie("authorizationBearer", token, myUri.path, myUri.host, null, -1, true, true))
                 .build()
     }
