87f8f60026b8b52928c85ef1d932d2341d44e1b9
Long <khoathanglong@gmail.com>
2019-09-03 15:58:08 +0200

    move all env variables to config file

---
+++ radar-upload-frontend/src/app.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+
+export const clientId = process.env.VUE_APP_CLIENT_ID || $VUE_APP_CLIENT_ID;
+export const authAPI = process.env.VUE_APP_AUTH_API || $VUE_APP_AUTH_API;
+export const authCallback = process.env.VUE_APP_AUTH_CALLBACK || $VUE_APP_AUTH_CALLBACK;
+export const baseURL = process.env.VUE_APP_BASE_URL || $VUE_APP_BASE_URL;
+
+export default {
+  clientId,
+  authAPI,
+  authCallback,
+  baseURL,
+};
