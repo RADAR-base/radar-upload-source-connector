61c3c813227da61275f6e4f4116e75367cf734e1
nivethika <nivemaham@gmail.com>
2019-09-13 14:04:34 +0200

    configurable settings for frontend

--- radar-upload-frontend/src/app.config.js
+++ radar-upload-frontend/src/app.config.js
@@ -3,7 +3,7 @@
 export const clientId = process.env.VUE_APP_CLIENT_ID || $VUE_APP_CLIENT_ID;
 export const authAPI = process.env.VUE_APP_AUTH_API || $VUE_APP_AUTH_API;
 export const authCallback = process.env.VUE_APP_AUTH_CALLBACK || $VUE_APP_AUTH_CALLBACK;
-export const baseURL = process.env.VUE_APP_BASE_URL || $VUE_APP_BASE_URL;
+export const baseURL = process.env.VUE_APP_API_BASE_URL || VUE_APP_API_BASE_URL;
 
 export default {
   clientId,
