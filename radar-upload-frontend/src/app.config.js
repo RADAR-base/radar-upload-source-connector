ff7ba81ab6caa199cd0e0099a7883a590f9b4233
nivethika <nivemaham@gmail.com>
2019-09-17 15:16:45 +0200

    remove unnecessary variables.

--- radar-upload-frontend/src/app.config.js
+++ radar-upload-frontend/src/app.config.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 
-export const clientId = process.env.VUE_APP_CLIENT_ID || $VUE_APP_CLIENT_ID;
-export const authAPI = process.env.VUE_APP_AUTH_API || $VUE_APP_AUTH_API;
-export const authCallback = process.env.VUE_APP_AUTH_CALLBACK || $VUE_APP_AUTH_CALLBACK;
-export const baseURL = process.env.VUE_APP_API_BASE_URL || $VUE_APP_API_BASE_URL;
+export const clientId = process.env.VUE_APP_CLIENT_ID
+export const authAPI = process.env.VUE_APP_AUTH_API
+export const authCallback = process.env.VUE_APP_AUTH_CALLBACK
+export const baseURL = process.env.VUE_APP_API_BASE_URL
 
 export default {
   clientId,
