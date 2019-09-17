030b261d360390938cc14f60c8998d2eb40d24d7
Joris Borgdorff <github@blootsvoets.net>
2019-09-17 12:19:31 +0200

    Make the config compatible with frontend and backend

--- docker/frontend-entrypoint.sh
+++ docker/frontend-entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
+ROOT_DIR=/usr/share/nginx/html
+
 # Replace env vars in JavaScript files
 echo "Replacing env vars in JS"
-for file in /usr/share/nginx/html/js/app.*.js*;
+for file in $ROOT_DIR/js/app.*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
 do
   echo "Processing $file ...";
 
+  sed -i 's|/\?VUE_APP_BASE_URL|'${VUE_APP_BASE_URL}'|g' $file
   sed -i 's|VUE_APP_API_BASE_URL|'${VUE_APP_API_BASE_URL}'|g' $file
   sed -i 's|VUE_APP_AUTH_API|'${VUE_APP_AUTH_API}'|g' $file
   sed -i 's|VUE_APP_AUTH_CALLBACK|'${VUE_APP_AUTH_CALLBACK}'|g' $file
   sed -i 's|VUE_APP_CLIENT_ID|'${VUE_APP_CLIENT_ID}'|g' $file
-
 done
 
 echo "Starting Nginx"
