9ee859314474f8878a7babd319b983f79b76edcc
Long <khoathanglong@gmail.com>
2019-09-17 11:52:18 +0200

    add file to record list

--- radar-upload-frontend/src/components/Record/RecordTable.vue
+++ radar-upload-frontend/src/components/Record/RecordTable.vue
@@ -4,12 +4,71 @@
     :items="recordList"
     :loading="loading"
     :search="searchText"
+    single-expand
+    item-key="id"
+    :expanded.sync="expandedItems"
+    show-expand
+    @click:row="expandRow"
   >
     <template #item.uploadedAt="{item}">
       <td
         class="pl-0 pb-0"
       >
         {{ item.modifiedDate | localTime }}
+      </td>
+    </template>
+
+    <!-- file list -->
+    <template #expanded-item="{item}">
+      <td
+        :colspan="$vuetify.breakpoint.name==='xs' ? 0 :12"
+        class="pa-2"
+      >
+        <v-list-item
+          v-for="(file,fileIndex) in item.files"
+          :key="fileIndex"
+        >
+          <v-list-item-avatar>
+            <v-icon>
+              mdi-file
+            </v-icon>
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title v-text="file.fileName" />
+            <v-list-item-subtitle>Size: {{ file.size }} Bytes</v-list-item-subtitle>
+            <v-list-item-subtitle>{{ file.createdDate | localTime }}</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-menu
+              open-on-hover
+              bottom
+              offset-y
+            >
+              <template #activator="{on}">
+                <v-btn
+                  icon
+                  v-on="on"
+                >
+                  <v-icon color="primary">
+                    mdi-dots-vertical
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-card>
+                <v-list
+                  shaped
+                >
+                  <v-list-item-group color="primary lighten-2">
+                    <v-list-item @click="downloadFile(item.id, file.fileName)">
+                      Download
+                    </v-list-item>
+                  </v-list-item-group>
+                </v-list>
+              </v-card>
+            </v-menu>
+          </v-list-item-action>
+        </v-list-item>
       </td>
     </template>
   </v-data-table>
@@ -37,6 +96,7 @@ export default {
       ],
       recordList: [
       ],
+      expandedItems: [],
     };
   },
   computed: {
@@ -69,12 +129,23 @@ export default {
     async getRecordList(projectId) {
       this.recordList = [];
       this.loading = true;
-      const recordList = await fileAPI.filterRecords({ projectId, getRecordOnly: true })
+      const recordList = await fileAPI.filterRecords({ projectId })
         .catch(() => []);
       this.loading = false;
       this.recordList = recordList;
     },
+    async expandRow(row) {
+      if (this.expandedItems[0] && (this.expandedItems[0].id === row.id)) {
+        this.expandedItems = [];
+      } else {
+        this.expandedItems.splice(0, 1, row);
+      }
+    },
+    downloadFile(recordId, fileName) {
+      fileAPI.download({ recordId, fileName });
+    },
   },
+
 };
 </script>
 
