3d165088a1c3a5268e4849b81bcb672590d998d2
nivethika <nivemaham@gmail.com>
2019-12-05 10:52:32 +0100

    map to ENUM based on the spec

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/altoida/summary/AltoidaSummaryProcessor.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/altoida/summary/AltoidaSummaryProcessor.kt
@@ -13,10 +13,40 @@ class AltoidaSummaryProcessor : AltoidaSummaryLineToRecordMapper {
                 line["LABEL"],
                 line.getValue("AGE").toInt(),
                 line.getValue("YEARSOFEDUCATION").toInt(),
-                line.getValue("GENDER").toInt(),
-                line.getValue("CLASS").toInt(),
+                line.getValue("GENDER").toInt().toGender(),
+                line.getValue("CLASS").toInt().classify(),
                 line.getValue("NMI").toDouble(),
-                line.getValue("GROUNDTRUTH").toInt()
+                line.getValue("GROUNDTRUTH").toInt().toGroundTruth()
         )
     }
+
+    private fun Int.toGender() : GenderType {
+        return when (this) {
+            0 -> GenderType.MALE
+            1 -> GenderType.FEMALE
+            2 -> GenderType.OTHER
+            else -> GenderType.UNKNOWN
+        }
+    }
+
+    private fun Int.classify() : Classification? {
+        return when (this) {
+            0 -> Classification.HEALTHY
+            1 -> Classification.AT_RISK
+            2 -> Classification.MCI_DUE_TO_AD
+            else -> null
+        }
+    }
+
+    private fun Int.toGroundTruth() : GroundTruth? {
+        return when (this) {
+            -1 -> GroundTruth.UNKNOWN
+            0 -> GroundTruth.HEALTHY
+            1 -> GroundTruth.AT_RISK
+            2 -> GroundTruth.MCI_OR_AB_PLUS
+            3 -> GroundTruth.MCI_OR_AB_MINUS
+            else -> GroundTruth.UNKNOWN
+        }
+    }
+
 }
