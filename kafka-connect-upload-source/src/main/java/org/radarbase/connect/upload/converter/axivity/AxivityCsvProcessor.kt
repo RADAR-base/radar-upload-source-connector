2d794a2d719c3c54be8b8d83b9282369a1ec4672
nivethika <nivemaham@gmail.com>
2019-12-30 13:07:33 +0100

    move cwaoption processing to converter factory

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/axivity/AxivityCsvProcessor.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/axivity/AxivityCsvProcessor.kt
@@ -2,12 +2,10 @@ package org.radarbase.connect.upload.converter.axivity
 
 import org.radarbase.connect.upload.api.ContentsDTO
 import org.radarbase.connect.upload.converter.*
-import org.radarbase.connect.upload.converter.axivity.CwaFileProcessorFactory.Companion.CWA_HEADER
 
-class AxivityCsvLineProcessor : OneToManyCsvProcessor() {
+class AxivityCsvProcessor(override val header: List<String>) : OneToManyCsvProcessor() {
     override val fileNameSuffix: String = ".cwa"
     override fun matches(contents: ContentsDTO): Boolean = contents.fileName.endsWith(fileNameSuffix, true)
-    override val header: List<String> = CWA_HEADER
     override val recordMappers: List<LineToRecordMapper>
         get() = listOf(
             AxivityAccelerationProcessor()
