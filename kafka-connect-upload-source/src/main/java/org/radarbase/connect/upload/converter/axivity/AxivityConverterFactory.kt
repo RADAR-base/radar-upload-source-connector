51909ebb6323e0cf6619e363dc92791d90914f4f
nivethika <nivemaham@gmail.com>
2019-12-30 13:07:33 +0100

    move cwaoption processing to converter factory

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/axivity/AxivityConverterFactory.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/axivity/AxivityConverterFactory.kt
@@ -5,6 +5,8 @@ import org.radarbase.connect.upload.converter.ConverterFactory
 import org.radarbase.connect.upload.converter.FileProcessorFactory
 import org.radarbase.connect.upload.converter.LogRepository
 import org.radarbase.connect.upload.converter.ZipFileProcessorFactory
+import org.radarbase.connect.upload.converter.axivity.newcastle.CwaCsvInputStream
+import org.slf4j.LoggerFactory
 
 class AxivityConverterFactory : ConverterFactory {
     override val sourceType: String = "axivity"
@@ -14,14 +16,54 @@ class AxivityConverterFactory : ConverterFactory {
             connectorConfig: SourceTypeDTO,
             logRepository: LogRepository
     ): List<FileProcessorFactory> {
+        checkNotNull(connectorConfig.configuration)
+        val (readOption, header) = cwaReadOptions(connectorConfig)
         return listOf(
                 ZipFileProcessorFactory(
                         listOf(
                                 CwaFileProcessorFactory(
-                                        listOf(AxivityCsvLineProcessor()),
+                                        listOf(AxivityCsvProcessor(header)),
                                         logRepository,
-                                        connectorConfig)),
+                                        header,
+                                        readOption)),
                         logRepository))
+    }
+
+    private fun cwaReadOptions(connectorConfig: SourceTypeDTO): Pair<Int, List<String>> {
+        // default to option 0 (Acceleration data)
+        val readOption = 0
+        val cwaHeader = mutableListOf("TIMESTAMP", "X", "Y", "Z")
+
+        // optionally read additional data based on the config
+        // this will compute the corresponding value for option and headers for the csv line that will be created.
+        val readAlso = connectorConfig.configuration?.getOrDefault("readAlso", null)
+        logger.info("Reading $readAlso from a cwa file.")
+        readAlso?.split(" ")?.forEach {
+            when (it) {
+                "--temp" -> {
+                    readOption.or(CwaCsvInputStream.OPTIONS_TEMP)
+                    cwaHeader.add("TEMPERATURE")
+                }
+                "--light" -> {
+                    readOption.or(CwaCsvInputStream.OPTIONS_LIGHT)
+                    cwaHeader.add("LIGHT")
+                }
+                "--battery" -> {
+                    readOption.or(CwaCsvInputStream.OPTIONS_BATT)
+                    cwaHeader.add("BATTERY")
+                }
+                "--events" -> {
+                    readOption.or(CwaCsvInputStream.OPTIONS_EVENTS)
+                    cwaHeader.add("EVENTS")
+                }
+            }
+        }
+        logger.info("Setting CWA read option to $readOption and headers to $cwaHeader")
+        return Pair(readOption, cwaHeader)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(CwaFileProcessorFactory.CwaProcessor::class.java)
     }
 }
 
--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/axivity/CwaFileProcessorFactory.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/axivity/CwaFileProcessorFactory.kt
@@ -2,7 +2,6 @@ package org.radarbase.connect.upload.converter.axivity
 
 import org.radarbase.connect.upload.api.ContentsDTO
 import org.radarbase.connect.upload.api.RecordDTO
-import org.radarbase.connect.upload.api.SourceTypeDTO
 import org.radarbase.connect.upload.converter.*
 import org.radarbase.connect.upload.converter.axivity.newcastle.CwaCsvInputStream
 import org.radarbase.connect.upload.exception.InvalidFormatException
@@ -12,7 +11,8 @@ import java.io.InputStream
 class CwaFileProcessorFactory(
         override val processorFactories: List<CsvLineProcessorFactory>,
         override val logRepository: LogRepository,
-        private val configuration: SourceTypeDTO
+        private val header: List<String>,
+        private val readOption: Int
 ) : CsvFileProcessorFactory(processorFactories, logRepository) {
 
     override fun createProcessor(record: RecordDTO) = CwaProcessor(record)
@@ -22,59 +22,24 @@ class CwaFileProcessorFactory(
         override fun convertLines(
                 contents: ContentsDTO,
                 inputStream: InputStream,
-                timeReceived: Double): List<FileProcessorFactory.TopicData> {
-            checkNotNull(configuration.configuration)
-            return readCsv(CwaCsvInputStream(inputStream, 0, 1, -1, cwaReadOptions).bufferedReader()) { reader ->
-                val header = CWA_HEADER
-                logger.info("Current Headers are $header")
-                val processorFactory = processorFactories
-                        .find { it.matches(contents) && it.matches(header) }
-                        ?: throw InvalidFormatException("In record ${record.id}, cannot find CSV processor that matches header $header")
+                timeReceived: Double): List<FileProcessorFactory.TopicData> = readCsv(CwaCsvInputStream(inputStream, 0, 1, -1, readOption).bufferedReader()) { reader ->
+            logger.info("Current Headers are $header")
+            val processorFactory = processorFactories
+                    .find { it.matches(contents) && it.matches(header) }
+                    ?: throw InvalidFormatException("In record ${record.id}, cannot find CSV processor that matches header $header")
 
-                val processor = processorFactory.createLineProcessor(record, logRepository)
+            val processor = processorFactory.createLineProcessor(record, logRepository)
 
-                generateSequence { reader.readNext() }
-                        .filter { processor.isLineValid(header, it) }
-                        .mapNotNull { processor.convertToRecord(header.zip(it).toMap(), timeReceived) }
-                        .toList()
-                        .flatten()
-            }
+            generateSequence { reader.readNext() }
+                    .filter { processor.isLineValid(header, it) }
+                    .mapNotNull { processor.convertToRecord(header.zip(it).toMap(), timeReceived) }
+                    .toList()
+                    .flatten()
         }
 
-
-        val cwaReadOptions: Int
-            get() {
-                val readAlso = configuration.configuration?.getOrDefault("readAlso", null) ?: return 0
-                logger.info("Reading $readAlso from a cwa file.")
-                val readValues = readAlso.split(" ")
-                val readOption = 0
-                readValues.forEach {
-                    when (it) {
-                        "--temp" -> {
-                            readOption.or(CwaCsvInputStream.OPTIONS_TEMP)
-                            CWA_HEADER.add("TEMPERATURE")
-                        }
-                        "--light" -> {
-                            readOption.or(CwaCsvInputStream.OPTIONS_LIGHT)
-                            CWA_HEADER.add("LIGHT")
-                        }
-                        "--battery" -> {
-                            readOption.or(CwaCsvInputStream.OPTIONS_BATT)
-                            CWA_HEADER.add("BATTERY")
-                        }
-                        "--events" -> {
-                            readOption.or(CwaCsvInputStream.OPTIONS_EVENTS)
-                            CWA_HEADER.add("EVENTS")
-                        }
-                    }
-                }
-                logger.info("Setting CWA read option to $readOption and headers to $CWA_HEADER")
-                return readOption
-            }
     }
 
     companion object {
         private val logger = LoggerFactory.getLogger(CwaProcessor::class.java)
-        val CWA_HEADER = mutableListOf("TIMESTAMP", "X", "Y", "Z")
     }
 }
