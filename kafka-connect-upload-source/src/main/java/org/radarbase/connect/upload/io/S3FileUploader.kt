a1ecb955201a8c8489b7b00778029d1dcf206e00
Joris Borgdorff <joris@thehyve.nl>
2020-08-03 12:43:30 +0200

    Update dependencies

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/io/S3FileUploader.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/io/S3FileUploader.kt
@@ -1,16 +1,16 @@
 package org.radarbase.connect.upload.io
 
-import io.minio.MinioClient
-import io.minio.PutObjectOptions
+import io.minio.*
+import io.minio.errors.MinioException
 import org.slf4j.LoggerFactory
+import java.io.IOException
 import java.io.InputStream
 import java.net.ConnectException
 import java.net.URI
 import java.nio.file.Path
 
 class S3FileUploader(override val config: FileUploaderFactory.FileUploaderConfig) : FileUploaderFactory.FileUploader {
-    override val type: String
-        get() = "s3"
+    override val type: String = "s3"
     private val s3Client: MinioClient
     private val bucket: String
     init {
@@ -18,36 +18,55 @@ class S3FileUploader(override val config: FileUploaderFactory.FileUploaderConfig
         config.username ?: throw ConnectException("upload.source.file.uploader.username must be configured for one or more of the selected converters")
         config.password ?: throw ConnectException("upload.source.file.uploader.password must be configured for one or more of the selected converters")
 
-        s3Client = try {
-            MinioClient(config.targetEndpoint, config.username, config.password)
-        } catch (ex: IllegalArgumentException) {
-            logger.warn("Invalid S3 configuration provided for $config.targetEndpoint", ex)
-            throw ex
-        }
-        bucket = config.targetRoot
-        logger.info("Object storage configured with endpoint {} in bucket {}",
-                config.targetEndpoint, bucket)
+        try {
+            s3Client = try {
+                MinioClient.Builder()
+                        .endpoint(config.targetEndpoint)
+                        .credentials(config.username, config.password)
+                        .build()
+            } catch (ex: IllegalArgumentException) {
+                logger.warn("Invalid S3 configuration provided for $config.targetEndpoint", ex)
+                throw ex
+            }
+            bucket = config.targetRoot
+            logger.info("Object storage configured with endpoint {} in bucket {}",
+                    config.targetEndpoint, bucket)
 
-        // Check if the bucket already exists.
-        val isExist: Boolean = s3Client.bucketExists(bucket)
-        if (isExist) {
-            logger.info("Bucket $bucket already exists.")
-        } else {
-            s3Client.makeBucket(bucket)
-            logger.info("Bucket $bucket was created.")
+            // Check if the bucket already exists.
+            val isExist: Boolean = s3Client.bucketExists(BucketExistsArgs.Builder()
+                    .bucket(bucket)
+                    .build())
+            if (isExist) {
+                logger.info("Bucket $bucket already exists.")
+            } else {
+                s3Client.makeBucket(MakeBucketArgs.Builder()
+                        .bucket(bucket)
+                        .build())
+                logger.info("Bucket $bucket was created.")
+            }
+        } catch (ex: MinioException) {
+            throw IOException(ex)
         }
     }
 
     override fun upload(relativePath: Path, stream: InputStream, size: Long?) : URI {
-        logger.info("Uploading object ${relativePath} to $bucket")
-        val putObjectOptions = if (size == null) PutObjectOptions(-1, (5*1024*2014)) else PutObjectOptions(size, -1)
-        s3Client.putObject(bucket, relativePath.toString(), stream, putObjectOptions)
+        logger.info("Uploading object $relativePath to $bucket")
+        try {
+            s3Client.putObject(PutObjectArgs.Builder()
+                    .bucket(bucket)
+                    .`object`(relativePath.toString())
+                    .stream(stream, size ?: -1, 5 * 1024 * 2014)
+                    .build())
+        } catch (ex: MinioException) {
+            throw IOException(ex)
+        }
+
         return advertisedTargetUri().resolve(rootDirectory().resolve(relativePath).toString())
     }
 
     override fun close() {
     }
-    
+
     companion object {
         private val logger = LoggerFactory.getLogger(S3FileUploader::class.java)
     }
