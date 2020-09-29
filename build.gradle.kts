abb92e9e4ca7af61c2785005242e510ed2c873f1
Joris Borgdorff <joris@thehyve.nl>
2020-09-29 12:33:15 +0200

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,9 @@ plugins {
     kotlin("jvm") apply false
 }
 
-
 subprojects {
     group = "org.radarbase"
-    version = "0.5.4-SNAPSHOT"
+    version = "0.5.4"
 
     project.extra.apply {
         set("kafkaVersion", "2.5.1")
