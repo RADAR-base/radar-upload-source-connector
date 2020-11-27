f469abec524392138b0d957dec463225cc855942
mpgxvii <mpgxvii@gmail.com>
2020-11-27 10:50:32 +0800

    Proceed processing even if line and header size do not match

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/CsvLineProcessorFactory.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/CsvLineProcessorFactory.kt
@@ -69,8 +69,8 @@ interface CsvLineProcessorFactory {
                     false
                 }
                 header.size != line.size -> {
-                    recordLogger.warn("Line size ${line.size} does not match with header size ${header.size}. Skipping this line")
-                    false
+                    recordLogger.warn("Line size ${line.size} does not match with header size ${header.size}. Still proceeding")
+                    true
                 }
                 line.any { it.isEmpty() } -> {
                     recordLogger.warn("Line with empty values found. Skipping this line")
