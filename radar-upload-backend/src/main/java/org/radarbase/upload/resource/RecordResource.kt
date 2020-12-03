b2a98e3c0727fe7598fea0f66bf9370da79fab46
mpgxvii <mpgxvii@gmail.com>
2020-12-03 22:15:09 +0800

    Update RecordResource to allow resetting record status to READY

--- radar-upload-backend/src/main/java/org/radarbase/upload/resource/RecordResource.kt
+++ radar-upload-backend/src/main/java/org/radarbase/upload/resource/RecordResource.kt
@@ -281,6 +281,20 @@ class RecordResource(
                 .also { callbackManager.callback(it) }
     }
 
+    @POST
+    @Path("{recordId}/retry")
+    fun resetRecordStatus(
+            @PathParam("recordId") recordId: Long,
+            @Context callbackManager: CallbackManager): RecordMetadataDTO {
+        val record = ensureRecord(recordId)
+        val recordDto = recordMapper.fromRecord(record)
+        recordDto.metadata?.status = RecordStatus.READY.toString()
+
+        val updatedRecord = recordRepository.updateMetadata(recordId, recordDto.metadata!!)
+        return recordMapper.fromMetadata(updatedRecord)
+                .also { callbackManager.callback(it) }
+    }
+
     @GET
     @Path("{recordId}/logs")
     fun getRecordLogs(
