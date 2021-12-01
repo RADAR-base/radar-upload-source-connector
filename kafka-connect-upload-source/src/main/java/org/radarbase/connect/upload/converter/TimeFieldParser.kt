bbf537207d777cead6bf1c5c246e3739ebb4d0dd
mpgxvii <mpgxvii@gmail.com>
2021-12-01 16:26:24 +0000

    Update timefieldparser

--- kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/TimeFieldParser.kt
+++ kafka-connect-upload-source/src/main/java/org/radarbase/connect/upload/converter/TimeFieldParser.kt
@@ -7,17 +7,26 @@ import java.time.format.DateTimeFormatter
 /** Parser for CSV time fields */
 interface TimeFieldParser {
     fun time(line: Map<String, String>): Double
+    fun timeFromString(timestamp: String): Double
 
     /** CSV timestamp parser, assuming that the timestamp is provided as milliseconds since the Unix Epoch. */
     class EpochMillisParser(private val fieldName: String = "TIMESTAMP") : TimeFieldParser {
         override fun time(line: Map<String, String>): Double =
                 line.getValue(fieldName).toDouble() / 1000.0
+
+        override fun timeFromString(timestamp: String): Double {
+            TODO("Not yet implemented")
+        }
     }
 
     /** CSV timestamp parser using given date-time format. */
     class DateFormatParser(private val timeFormatter: DateTimeFormatter, private val fieldName: String = "TIMESTAMP") : TimeFieldParser {
         override fun time(line: Map<String, String>): Double =
                 Instant.from(timeFormatter.parse(line.getValue(fieldName)))
+                        .toEpochMilli() / 1000.0
+
+        override fun timeFromString(timestamp: String): Double =
+                Instant.from(timeFormatter.parse(timestamp))
                         .toEpochMilli() / 1000.0
 
         companion object {
