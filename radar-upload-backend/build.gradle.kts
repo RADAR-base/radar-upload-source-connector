8faa1bdf4e77c0a89f6e90bb11b6ba1af77e27b7
pvannierop <pim@thehyve.nl>
2024-10-04 16:08:00 +0200

    Add support for Sentry monitoring
    Includes addition to documentation.

--- radar-upload-backend/build.gradle.kts
+++ radar-upload-backend/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     id("org.jetbrains.kotlin.plugin.noarg")
     id("org.jetbrains.kotlin.plugin.jpa")
     id("org.jetbrains.kotlin.plugin.allopen")
+    // TODO Remove this when new release of radar-commons is available and used in this project.
+    // This version has Sentry support built in for radar-kotlin plugin.
+    id("io.sentry.jvm.gradle") version "4.11.0"
 }
 
 application {
